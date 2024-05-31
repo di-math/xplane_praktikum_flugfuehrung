de3673fc279e906a2d7e712d9adb83dcf9daebc9
Simon Demuth <60353189+di-math@users.noreply.github.com>
2024-05-31 15:57:15 +0200

    Update map.js

--- map/map.js
+++ map/map.js
@@ -66,3 +66,38 @@ function get_pixels_from_geo_coordinates(lat, lon) {
   y = map(lon, ref_a[3], ref_b[3], ref_a[1], ref_b[1]);
   return [x, y]
 }
+
+
+
+import spc
+from time import sleep
+import socket
+import json
+import asyncio
+import websockets
+
+PORT = 3000
+clients = set()
+
+async def connection_callback(websocket):
+    clients.add(websocket)
+    async for msg in websocket:
+        print(msg)
+    
+
+async def send_pos():
+    with spc.FlightController() as fc:
+        while True:
+            if clients:
+                pos = fc.xpc.getPOSI()
+                await asyncio.gather(*[client.send(json.dumps({"lat": pos[1], "lon": pos[0], "heading": pos[5]})) for client in clients])
+            await asyncio.sleep(1)
+    
+
+async def main():
+    async with websockets.serve(connection_callback, "localhost", 3000):
+        await asyncio.gather(send_pos())
+        
+
+if __name__ == "__main__":
+    asyncio.run(main())
